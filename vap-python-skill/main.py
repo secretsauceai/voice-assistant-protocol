d8075f02874ebb6834081bc44d7c2bf54fd4e188
Sergio Tortosa <sertorbe@gmail.com>
2022-02-14 16:18:12 +0100

    Notification and query in Python

--- vap-python-skill/main.py
+++ vap-python-skill/main.py
@@ -21,7 +21,7 @@ class VapRequestResource(resource.Resource):
         print(f"Received a request with capabilities: {caps}")
 
         if payload["request"]["intent"] == "hello":
-            data_response = {"capabilities": [{"text": "hello there"}]}
+            data_response = {"capabilities": [{"name":"text", "text": "hello there"}]}
             return aiocoap.Message(payload=msgpack.packb(data_response))
             
 
@@ -38,9 +38,14 @@ class VapCanYouAnswerResource(resource.Resource):
 
 class VapClient():
     async def _init(self):
+        # This is not __init__, we can't do this in _init because it is async,
+        # this needs to be called afterwards
+
         self.context = await aiocoap.Context.create_client_context()
     
     async def init(self):
+        # Register whithin the server
+
         payload = {
             "name": "My test skill",
             "id": "com.example.test",
@@ -58,6 +63,8 @@ class VapClient():
         print(f"Languages available: {','.join(resp_payload['languages'])}")
 
     async def registerUtts(self):
+        # Send our utterances to the server for them to be taken account of
+
         payload = {
             "nluData": [
                 {
@@ -95,6 +102,8 @@ class VapClient():
             raise Exception(f"Failed to register utterances: {response.code}")
 
     async def close(self):
+        # We have finished, let it know to the server
+
         payload = {"skillId":"com.example.test"}
         request = aiocoap.Message(code=aiocoap.GET, payload=msgpack.packb(payload), uri=f'coap://{registry_address}/vap/skillRegistry/skillClose')
 
@@ -103,8 +112,47 @@ class VapClient():
         if response.code != aiocoap.DELETED:
             raise Exception(f"Failed to register utterances: {response.code}")
 
+    async def notification(self):
+        # Some request started by the skill, right now doesn't return a response.
 
+        payload = {
+            "data":[{
+                "clientId": "123456789a",
+                "capabilities": [{
+                    "name": "text",
+                    "text": "hello there"
+                }]
+            }]
+        }
 
+        request = aiocoap.Message(code=aiocoap.GET, payload=msgpack.packb(payload), uri=f'coap://{registry_address}/vap/skillRegistry/notification')
+
+        await self.context.request(request)
+
+    async def query(self):
+        # Ask something to the system about a certain client or about the system itself.
+
+        payload = {
+            "data":[{
+                "clientId": "123456789a",
+                "capabilities": [{
+                    "name": "preferences",
+                    "what": "color"
+                }]
+            }]
+        }
+
+        request = aiocoap.Message(code=aiocoap.GET, payload=msgpack.packb(payload), uri=f'coap://{registry_address}/vap/skillRegistry/notification')
+
+        response = await self.context.request(request).response
+
+        cap_color = filter(
+            lambda c: c["name"]=="preferences",
+            msgpack.unpackb(response)["data"][0]["capabilities"])
+
+        color = cap_color[0]["color"]
+
+        print(f"Preferred color: {color}")
     
 client = VapClient()
 
@@ -122,6 +170,10 @@ async def main():
     root.add_resource((['vap','canYouAnswer']), VapCanYouAnswerResource())
 
     await aiocoap.Context.create_server_context(root)
+
+    # Perform notifications and queries, note this can be done whenever
+    await client.notification()
+    await client.query()
 
     # Run forever
     await asyncio.get_running_loop().create_future()
