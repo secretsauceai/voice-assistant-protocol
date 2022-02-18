b65ceb27297a5b67a9feb22c0289e4c818f7c27a
Sergio Tortosa "sheosi <sertorbe@gmail.com>
2022-02-18 12:51:47 +0100

    Add real world data and adapt for Hermes

--- benchmarks/main.py
+++ benchmarks/main.py
@@ -5,25 +5,54 @@ import protos
 from os import urandom
 
 SAMPLE_RATE = 48000
-SAMPLES_PER_MS = SAMPLE_RATE / 1000
+SAMPLES_PER_MS = int(SAMPLE_RATE / 1000)
+
+client = "org.mycompany.myclient.134567"
 
 def make_sound(ms: int = 0) -> bytes:
     return bytearray(urandom(ms * SAMPLES_PER_MS))
 
 @benchwork.benchmark(with_classes=protos.all_protos)
 def voice_send(proto: protos.VaProto):
-    proto.send_message("/asis_api/voice", {"voice": make_sound(ms=200)})
+    proto.send_message("/asis_api/voice/", client, make_sound(ms=200))
 
 @benchwork.benchmark(with_classes=protos.all_protos)
 def sound_send(proto: protos.VaProto):
-    proto.send_message("/asis_api/sound", {"sound": make_sound(ms=2_000)})
+    proto.send_message("/asis_api/sound/", client, make_sound(ms=2_000))
 
 @benchwork.benchmark(with_classes=protos.all_protos)
 def large_sound(proto: protos.VaProto):
-    proto.recv_message("/asis_api/sound", {"sound": make_sound(ms=50_000)})
+    proto.send_message("/asis_api/sound/", client, make_sound(ms=50_000))
 
 @benchwork.benchmark(with_classes=protos.all_protos)
 def skill_answer(proto: protos.VaProto):
-    proto.send_message("/asis_api/data", {"TODO": "DATA"})
+    proto.send_message("/asis_api/intent/request_weather", client, {
+        "input": "what is the weather like in london, england tomorrow?",
+        "intent": {
+            "intentName": "request_weather",
+            "confidenceScore": 0.8564,
+        },
+        "slots": [
+            {
+                "entity": "location",
+                "slot": "place",
+                "confidence": 0.687,
+                "rawValue": "london, england",
+                "value": {
+                    "latitude": 51.5073,
+                    "longitude": -0.1277,
+                },
+                "range": {
+                    "start": 28,
+                    "end": 43
+                }
+            }
+        ],
+        "id": "13546789",
+        "siteId": client,
+        "sessionId": "1234a56d3c679e",
+        "asrConfidence": 0.678,
+
+    })
 
 benchwork.run()
--- benchmarks/protos.py
+++ benchmarks/protos.py
@@ -1,14 +1,20 @@
-from typing import Any, Dict, Protocol
+from ast import Call
+from typing import Any, Callable, Dict, Protocol, Union
 import asyncio
 
-import paho.mqtt.client as mqtt
-import mqttsn.client
+# Transport protocols
 import aiocoap
+import mqttsn.client
+import paho.mqtt.client as mqtt
+
+# Message formats
 import msgpack
 from flatbuffers import flexbuffers
+from hivemind_bus_client import HiveMessageBusClient
+import json
 from mapbuffer import MapBuffer
 import mycroft_bus_client as mycroft
-from hivemind_bus_client import HiveMessageBusClient
+
 
 MQTT_SERVER_IP = ""
 MQTT_SEVER_PORT = 1883
@@ -19,86 +25,101 @@ MQTTSN_SERVER_PORT = 1883
 COAP_SERVER_IP = ""
 COAP_SERVER_PORT = 5683
 
+SendData = Union[Dict[str, Any],  bytes]
+
+def encapsulate_bin(data: SendData, f: Callable[[Dict[str, Any]], bytes]) -> bytes:
+    if isinstance(data, dict):
+        return f(data)
+    return f({"voice": data})
+
 class VaProto(Protocol):
-    def send_message(self, dest: str, data: Dict[str, Any]):
-        # ...
-        pass
+    """Interface for all protocol implementations. Not to be used directly"""
+
+    def send_message(self, path: str, dest: str, data: SendData):
+        raise NotImplementedError()
 
 class MqttBase(VaProto):
+    """Base class for MQTT-based protocols. Not to be used directly"""
+
     def __init__(self):
         self.client = mqtt.Client()
 
         self.client.connect(MQTT_SERVER_IP, MQTT_SEVER_PORT)
     
-    def send_message(self, dest: str, data: Dict[str, Any]):
-        msg = self.client.publish(dest, paylooad=self.transform_payload(data))
+    def send_message(self, path: str, dest: str, data: SendData):
+        msg = self.client.publish(path + "/" + dest, paylooad=self.transform_payload(data))
         msg.wait_for_publish()
 
-    def transform_payload(self, data: Dict[str, Any]) -> str:
+    def transform_payload(self, data: SendData) -> bytes:
+        """Transform the payload to a string to be inserted in the message"""
         raise NotImplementedError()
 
 class MqttSnBase(VaProto):
+    """Base class for MQTT-SN-based protocols. Not to be used directly"""
+
     def __init__(self):
         self.client = mqttsn.client.Client(MQTTSN_SERVER_IP, MQTTSN_SERVER_PORT)
         self.client.connect()
 
-    def send_message(self, dest: str, data: Dict[str, Any]):
-        self.client.publish(dest, self.transform_payload(data))
+    def send_message(self, path: str, dest: str, data: SendData):
+        self.client.publish(path + "/" + dest, self.transform_payload(data))
     
-    def transform_payload(self, data: Dict[str, Any]) -> str:
+    def transform_payload(self, data: SendData) -> bytes:
         raise NotImplementedError()
 
 class CoApBase(VaProto):
+    """Base class for CoAP-based protocols. Not to be used directly"""
+
     def __init__(self):
         self.client = asyncio.get_event_loop().run_until_complete(aiocoap.Context.create_client_context())
     
-    def send_message(self, dest: str, data: Dict[str, Any]):
-        request = aiocoap.Message(code=aiocoap.POST, uri=COAP_SERVER_IP + '/' + dest, payload=self.transform_payload(data))
+    def send_message(self, path: str, dest: str, data: SendData):
+        request = aiocoap.Message(code=aiocoap.POST, uri=COAP_SERVER_IP + '/' + path, payload=self.transform_payload(data))
         asyncio.get_event_loop().run_until_complete(self.client.request(request))    
     
-    def transform_payload(self, data: Dict[str, Any]) -> str:
+    def transform_payload(self, data: SendData) -> bytes:
         raise NotImplementedError()
 
 # Protocols themselves
 
 ## MQTT
 class MqttFlexbuf(MqttBase):
-    def transform_payload(self, data: Dict[str, Any]) -> str:
-        return flexbuffers.Dumps(data)
+    def transform_payload(self, data: SendData) -> bytes:
+        return encapsulate_bin(data, lambda d: flexbuffers.Dump(d))
 
 class MqttMapBuf(MqttBase):
-    def transform_payload(self, data: Dict[str, Any]) -> str:
-        return MapBuffer(data).tobytes()
+    def transform_payload(self, data: SendData) -> bytes:
+        return encapsulate_bin(data, lambda d: MapBuffer(d).tobytes())
 
 class MqttMessagePack(MqttBase):
-    def transform_payload(self, data: Dict[str, Any]) -> str:
-        return msgpack.packb(data)
+    def transform_payload(self, data: SendData) -> bytes:
+        return encapsulate_bin(data, lambda d: msgpack.packb(d))
 
 ## MQTT-SN
 class MqttSnFlexbuf(MqttSnBase):
-    def transform_payload(self, data: Dict[str, Any]) -> str: 
-        return flexbuffers.Dumps(data)
+    def transform_payload(self, data: SendData) -> bytes: 
+        return encapsulate_bin(data, lambda d: flexbuffers.Dumps(data))
 
 class MqttSnMapBuf(MqttSnBase):
-    def transform_payload(self, data: Dict[str, Any]) -> str:
-        return MapBuffer(data).tobytes()
+    def transform_payload(self, data: SendData) -> bytes:
+        return encapsulate_bin(data, lambda d: MapBuffer(d).tobytes())
 
 class MqttSnMessagePack(MqttSnBase):
-    def transform_payload(self, data: Dict[str, Any]) -> str:
-        return msgpack.packb(data)
+    def transform_payload(self, data: SendData) -> bytes:
+        return encapsulate_bin(data, lambda d: msgpack.packb(d))
 
 ## CoAP
 class CoApFlexbuf(VaProto):
-    def transform_payload(self, data: Dict[str, Any]) -> str:
-        return flexbuffers.Dumps(data)
+    def transform_payload(self, data: SendData) -> bytes:
+        return encapsulate_bin(data, lambda d: flexbuffers.Dump(d))
 
 class CoApMapBuf(VaProto):
-    def transform_payload(self, data: Dict[str, Any]) -> str:
-        return MapBuffer(data).tobytes()
+    def transform_payload(self, data: SendData) -> bytes:
+        return encapsulate_bin(data, lambda d: MapBuffer(d).tobytes())
 
 class CoApMessagePack(VaProto):
-    def transform_payload(self, data: Dict[str, Any]) -> str:
-        return msgpack.packb(data)
+    def transform_payload(self, data: SendData) -> bytes:
+        return encapsulate_bin(data, lambda d: msgpack.packb(d))
 
 ## Others
 class Hermes(VaProto):
@@ -109,23 +130,28 @@ class Hermes(VaProto):
 
         self.client.connect(MQTT_SERVER_IP, MQTT_SEVER_PORT)
 
-    def send_message(self, dest: str, data: Dict[str, Any]):
-        msg = self.client.publish(self.transform_dest(dest), paylooad=self.transform_payload(data))
+    def send_message(self, path: str, dest: str, data: SendData):
+        msg = self.client.publish(self.transform_dest(path, dest), paylooad=self.transform_payload(data))
         msg.wait_for_publish()
 
-    def transform_payload(self, data: Dict[str, Any]) -> str:
-        raise NotImplementedError()
+    @staticmethod
+    def transform_payload(data: SendData) -> bytes:
+        def binary_or(data: SendData, f: Callable[[Dict[str, Any]], bytes]) -> bytes:
+            if isinstance(data, dict):
+                return f(data)
+            return data
+
+        return json.dumps(data).encode()
     
     @staticmethod
-    def transform_dest(dest: str) -> str:
-        if dest == "/asis_api/voice":
-            return "/hermes/..."
-        elif dest == "/asis_api/sound":
-            return "/hermes/..."
-        elif dest == "/asis_api/data":
-            return "/hermes/..."
+    def transform_dest(path:str, dest: str) -> str:
+        if path.endswith("voice/"):
+            return f"/hermes/audioServer/{dest}/audioFrame"
 
-        return "UNKNOWN!!"
+        if path.endswith("sound/"):
+            return f"hermes/audioServer/{dest}/playBytes/123456789"
+
+        return '/hermes' + path[:len('/asis_api')]  + '/' + dest
 
         
 
@@ -136,7 +162,7 @@ class HiveMind(VaProto):
 
         self.client = HiveMessageBusClient(key, crypto_key=crypto_key, ssl=False)
 
-    def send_message(self, dest: str, data: Dict[str, Any]):
+    def send_message(self, path: str, dest: str, data: SendData):
         mycroft_msg = mycroft.Message(dest, data)
         self.client.emit_mycroft(mycroft_msg)
 
