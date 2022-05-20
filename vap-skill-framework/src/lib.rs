299d817be971b49d46f22e7af329dd96ccbe3da9
Sergio Tortosa "sheosi <sertorbe@gmail.com>
2022-05-20 17:45:01 +0200

    Call register on new skill

--- vap-skill-framework/src/lib.rs
+++ vap-skill-framework/src/lib.rs
@@ -3,20 +3,26 @@ mod load;
 use std::{io::Cursor, path::Path};
 
 use coap::CoAPClient;
-use coap_lite::{RequestType as Method, ResponseType, MessageClass};
+use coap_lite::{MessageClass, RequestType as Method, ResponseType};
 use fluent_langneg::negotiate_languages;
 use futures::channel::mpsc;
 use serde::Serialize;
 use unic_langid::LanguageIdentifier;
-use vap_common_skill::structures::{*, msg_notification::Data, msg_query::QueryData, msg_skill_request::{RequestData, ClientData}};
+use vap_common_skill::structures::{
+    msg_notification::Data,
+    msg_query::QueryData,
+    msg_skill_request::{ClientData, RequestData},
+    *,
+};
+
+pub use vap_common_skill::structures::msg_skill_request::RequestDataKind;
 
 pub struct Skill {
     client: CoAPClient,
     id: String,
     langs: Vec<LanguageIdentifier>,
-    sender: mpsc::Sender<RegistryRequest>
+    sender: mpsc::Sender<RegistryRequest>,
 }
-
 
 impl Skill {
     fn get_address() -> String {
@@ -24,65 +30,91 @@ impl Skill {
         format!("127.0.0.1:{}", PORT)
     }
 
-    pub fn new<S1, S2, P>(name: S1, id: S2, intents: P) -> (Self , SkillIn)
-        where
+    pub fn new<S1, S2, P>(name: S1, id: S2, intents: P) -> (Self, SkillIn)
+    where
         S1: Into<String>,
         S2: Into<String>,
-        P: AsRef<Path> + Clone {
-
+        P: AsRef<Path> + Clone,
+    {
         let id_str = id.into();
         let payload = rmp_serde::to_vec_named(&MsgConnect {
             id: id_str.clone(),
             name: name.into(),
             vap_version: "Alpha".into(),
-            unique_authentication_token: Some("".into())
-        }).unwrap();
-        
-        let client = CoAPClient::new(Self::get_address()).unwrap();
-        let resp = client.request_path(
-            "vap/skillRegistry/connect",
-            Method::Put,
-            Some(payload),
-            None,
-        ).unwrap();
+            unique_authentication_token: Some("".into()),
+        })
+        .unwrap();
 
-        match resp.message.header.code  {
+        let client = CoAPClient::new(Self::get_address()).unwrap();
+        let resp = client
+            .request_path(
+                "vap/skillRegistry/connect",
+                Method::Put,
+                Some(payload),
+                None,
+            )
+            .unwrap();
+
+        match resp.message.header.code {
             MessageClass::Response(ResponseType::Created) => {
-                let payload: MsgConnectResponse = rmp_serde::from_read(Cursor::new(resp.message.payload)).unwrap();
+                let payload: MsgConnectResponse =
+                    rmp_serde::from_read(Cursor::new(resp.message.payload)).unwrap();
                 let (sender, receiver) = mpsc::channel(10);
-                let mut skill = Self {client, id: id_str, langs: payload.langs.into_iter().map(|l|l.into()).collect(), sender};
+                let mut skill = Self {
+                    client,
+                    id: id_str,
+                    langs: payload.langs.into_iter().map(|l| l.into()).collect(),
+                    sender,
+                };
                 skill.register_intents(intents);
-                
+                skill.register();
+
                 (skill, receiver)
-            },
-            _ => {panic!("ERROR")}
-        }        
+            }
+            _ => {
+                panic!("ERROR")
+            }
+        }
     }
 
-    fn send_message<T: Serialize>(&self, method: Method, path:&str, data: T) -> (ResponseType, Vec<u8>) {
+    fn send_message<T: Serialize>(
+        &self,
+        method: Method,
+        path: &str,
+        data: T,
+    ) -> (ResponseType, Vec<u8>) {
         let d = rmp_serde::to_vec_named(&data).unwrap();
-        let resp = self.client.request_path(path, method, Some(d), None).unwrap();
+        let resp = self
+            .client
+            .request_path(path, method, Some(d), None)
+            .unwrap();
         if let MessageClass::Response(c) = resp.message.header.code {
             (c, resp.message.payload)
-        }
-        else {
+        } else {
             panic!("Should be a response")
         }
     }
 
-    fn send_message_no_payload(&self, method: Method, path:&str) -> ResponseType {        
+    fn send_message_no_payload(&self, method: Method, path: &str) -> ResponseType {
         let resp = self.client.request_path(path, method, None, None).unwrap();
         if let MessageClass::Response(c) = resp.message.header.code {
             c
-        }
-        else {
+        } else {
             panic!("Should be a response")
         }
     }
 
-    pub fn register_intents<P>(&mut self, intents: P) where P: AsRef<Path> + Clone {
+    pub fn register_intents<P>(&mut self, intents: P)
+    where
+        P: AsRef<Path> + Clone,
+    {
         let langs = load::list_langs(intents.clone());
-        let langs = negotiate_languages(&self.langs, &langs, None, fluent_langneg::NegotiationStrategy::Matching);
+        let langs = negotiate_languages(
+            &self.langs,
+            &langs,
+            None,
+            fluent_langneg::NegotiationStrategy::Matching,
+        );
 
         let nlu_data = load::load_intents(&langs, intents);
 
@@ -91,36 +123,46 @@ impl Skill {
             "vap/skillRegistry/registerIntents",
             MsgRegisterIntents {
                 skill_id: self.id.clone(),
-                nlu_data
-            }) {
-                (ResponseType::Created,_) => {}
-                _ => panic!("Response not good")
-            }
+                nlu_data,
+            },
+        ) {
+            (ResponseType::Created, _) => {}
+            _ => panic!("Response not good"),
+        }
     }
 
     pub fn close(&mut self) {
         if self.send_message_no_payload(
             Method::Delete,
-            &format!("vap/skillRegistry/skills/{}", &self.id)
-        ) != ResponseType::Deleted {
+            &format!("vap/skillRegistry/skills/{}", &self.id),
+        ) != ResponseType::Deleted
+        {
             panic!("Response not good")
         }
     }
 
-    pub fn notify(&mut self, client_id: String, capabilities: Vec<PlainCapability>) -> MsgNotificationResponse {
-        self.notify_multiple(vec![Data::StandAlone{client_id, capabilities}])
+    pub fn notify(
+        &mut self,
+        client_id: String,
+        capabilities: Vec<PlainCapability>,
+    ) -> MsgNotificationResponse {
+        self.notify_multiple(vec![Data::StandAlone {
+            client_id,
+            capabilities,
+        }])
     }
 
     pub fn notify_multiple(&mut self, data: Vec<Data>) -> MsgNotificationResponse {
         match self.send_message(
             Method::Post,
             "vap/skillRegistry/notification",
-            MsgNotification {skill_id: self.id.clone(), data}
+            MsgNotification {
+                skill_id: self.id.clone(),
+                data,
+            },
         ) {
-            (ResponseType::Content, d) => {
-                rmp_serde::from_read(Cursor::new(d)).unwrap()
-            }
-            _ => panic!("Failed to send notification")
+            (ResponseType::Content, d) => rmp_serde::from_read(Cursor::new(d)).unwrap(),
+            _ => panic!("Failed to send notification"),
         }
     }
 
@@ -128,23 +170,35 @@ impl Skill {
         match self.send_message(
             Method::Get,
             "vap/skillRegistry/query",
-            MsgQuery {skill_id: self.id.clone(), data}
+            MsgQuery {
+                skill_id: self.id.clone(),
+                data,
+            },
         ) {
-            (ResponseType::Content, d) => {
-                rmp_serde::from_read(Cursor::new(d)).unwrap()
-            }
+            (ResponseType::Content, d) => rmp_serde::from_read(Cursor::new(d)).unwrap(),
 
-            _ => panic!("Failed to query data")
+            _ => panic!("Failed to query data"),
         }
     }
 
-    pub async fn register(&mut self) {
+    pub fn register(&mut self) {
         let mut sender = self.sender.clone();
-        self.client.observe(&format!("vap/skillRegistry/skills/{}", &self.id), move |m| {
-            let payload: MsgSkillRequest = rmp_serde::from_read(Cursor::new(m.payload)).unwrap();
-            
-            sender.try_send(RegistryRequest {client: payload.client, request: payload.request}).unwrap();
-        }).unwrap();
+        self.client
+            .observe(
+                &format!("vap/skillRegistry/skills/{}", &self.id),
+                move |m| {
+                    let payload: MsgSkillRequest =
+                        rmp_serde::from_read(Cursor::new(m.payload)).unwrap();
+
+                    sender
+                        .try_send(RegistryRequest {
+                            client: payload.client,
+                            request: payload.request,
+                        })
+                        .unwrap();
+                },
+            )
+            .unwrap();
     }
 }
 
@@ -156,7 +210,7 @@ impl Drop for Skill {
 
 pub struct RegistryRequest {
     pub client: ClientData,
-    pub request: RequestData
+    pub request: RequestData,
 }
 
 type SkillIn = mpsc::Receiver<RegistryRequest>;
@@ -164,10 +218,13 @@ type SkillIn = mpsc::Receiver<RegistryRequest>;
 #[cfg(test)]
 mod tests {
     use crate::Skill;
+    use futures::StreamExt;
 
-    #[test]
-    fn it_works() {
+    #[tokio::test]
+    async fn it_works() {
         let (mut skill, mut skill_in) = Skill::new("Test", "com.example.test", "assets");
-        skill.register();
+        loop {
+            let req = skill_in.next().await.unwrap();
+        }
     }
 }
